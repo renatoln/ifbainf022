6e63dd3f80a1a59360cae707cbe6b0adc8d2d368
Enoque Joseneas <enoquejoseneas@gmail.com>
2016-01-20 19:36:47 -0300

    O retorno do Aluno.java

--- Aluno.java
+++ Aluno.java
@@ -1,15 +1,11 @@
 //alunos INF022 2015.2
 
-<<<<<<< HEAD
-class Aluno(){
+class Aluno() {
 	void IniciarCAMILA(){
 	
 	}
-=======
-class Aluno() {
 
 	void iniciarEnoque() { }
->>>>>>> f0835ebd8da201dd8aad030f6dd324d39d648dab
 
 	void iniciarAmilton() {}
 }
