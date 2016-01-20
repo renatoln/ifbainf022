424fbab288ebc4424aa055968e8ab4da8dd991df
Veronica-Nascimento <velzinha93@gmail.com>
2016-01-20 19:39:19 -0200

    Novo commit

--- Aluno.java
+++ Aluno.java
@@ -1,13 +1,16 @@
 //novo arquivo Aluno de INF022
 
 class AlunoINF022{
-<<<<<<< HEAD
 
-    void iniciarLeticia() { }
-=======
+    	void iniciarLeticia() { }
+
 	void IniciarCAMILA(){
 	
 	}
 
->>>>>>> c7f4eb559de53c18b689a65a554139ed06c9c713
+	void IniciarVeronica(){
+
+	}
+
+
 }
